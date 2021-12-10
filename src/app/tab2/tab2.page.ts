fbde47944db2755cdbc8218ee35a75aeea004dc6
dominio\glugo <g.lugo@ibermatica.com>
2021-12-10 13:42:42 +0100

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserPhoto } from '../models/user-photo.model';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+public photoGallery: UserPhoto[] = [];
+
+constructor(private photoService: PhotoService) { }
+
+async ngOnInit() {
+  await this.photoService.loadSaved();
+  this.photoGallery = this.photoService.getPhotos();
+}
+
+addPhotoToGallery() {
+  this.photoService.addNewToGallery();
+}
 
 }
